bf9f76c73347c29e6e89697338d7707324a16454
Paul Mitsui <paul@3wells.net>
2024-11-13 10:35:08 -0800

    Combos and code-cleanup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/RobotHardware.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class RobotHardware {
+    private static RobotHardware instance = null;
+
+    private static final String FRONT_LEFT_MOTOR = "frontLeftMotor";
+    private static final String FRONT_RIGHT_MOTOR = "frontRightMotor";
+    private static final String BACK_LEFT_MOTOR = "backLeftMotor";
+    private static final String BACK_RIGHT_MOTOR = "backRightMotor";
+    private static final String TILT_MOTOR = "TiltMotor";
+    private static final String SLIDER_MOTOR = "SliderMotor";
+    private static final String LEFT_ARM = "leftArm";
+    private static final String RIGHT_ARM = "rightArm";
+    private static final String CLAW = "Claw";
+    private static final String ELBOW = "Elbow";
+    private static final String WRIST = "Wrist";
+
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor backLeftMotor;
+    public DcMotor backRightMotor;
+    public DcMotor tiltMotor;
+    public DcMotor sliderMotor;
+    public Servo leftArm;
+    public Servo rightArm;
+    public Servo claw;
+    public Servo elbow;
+    public Servo wrist;
+
+    public static double armUpPosition = 0.40;
+    public static double armDownPosition = 0.568;
+
+    public static int sliderTopPosition = -500;
+    public static int sliderBottomPosition = 10;
+
+
+    public static double clawOpenPosition = 0.5;
+    public static double clawClosedPosition = 1.0;
+
+    public static double elbowUpPosition = 0.55;
+    public static double elbowDownPosition = 0.25;
+
+    public static int TILT_UP_POSITION = 5;
+    public static int TILT_DOWN_POSITION = -665;
+
+    public static RobotHardware getInstance() {
+        if (instance == null) {
+            instance = new RobotHardware();
+        }
+
+        return instance;
+    }
+
+    public void init(HardwareMap hardwareMap) {
+        frontLeftMotor = hardwareMap.dcMotor.get(FRONT_LEFT_MOTOR);
+        frontRightMotor = hardwareMap.dcMotor.get(FRONT_RIGHT_MOTOR);
+        backLeftMotor = hardwareMap.dcMotor.get(BACK_LEFT_MOTOR);
+        backRightMotor = hardwareMap.dcMotor.get(BACK_RIGHT_MOTOR);
+        tiltMotor = hardwareMap.dcMotor.get(TILT_MOTOR);
+        sliderMotor = hardwareMap.dcMotor.get(SLIDER_MOTOR);
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Servo declaration
+        leftArm = hardwareMap.servo.get(LEFT_ARM);
+        rightArm = hardwareMap.servo.get(RIGHT_ARM);
+        claw = hardwareMap.servo.get(CLAW);
+        elbow = hardwareMap.servo.get(ELBOW);
+        wrist = hardwareMap.servo.get(WRIST);
+
+        tiltMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        tiltMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        sliderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        sliderMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Initialize positions
+        rightArm.setPosition(armUpPosition);
+        claw.setPosition(clawClosedPosition);
+        elbow.setPosition(elbowUpPosition);
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OutreachTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OutreachTeleop.java
@@ -5,152 +5,51 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.common.RobotHardware;
+
 @TeleOp
 public class OutreachTeleop extends LinearOpMode {
+    private static RobotHardware robot = RobotHardware.getInstance();
+
+    private static double speedLimit = 0.25;
+
+    private int sliderCurrentPosition = robot.sliderBottomPosition;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // Declare our motors
-        // Make sure your ID's match your configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        DcMotor tiltMotor = hardwareMap.dcMotor.get("TiltMotor");
-        DcMotor sliderMotor = hardwareMap.dcMotor.get("SliderMotor");
-        Servo leftArm = hardwareMap.servo.get("leftArm");
-        Servo rightArm = hardwareMap.servo.get("rightArm");
-        Servo Claw = hardwareMap.servo.get("Claw");
-        Servo Elbow = hardwareMap.servo.get("Elbow");
-        Servo Wrist = hardwareMap.servo.get("Wrist");
-        tiltMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        tiltMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        sliderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        sliderMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        int TiltUpPosition;
-        int TiltDownPosition;
-        int TopPosition;
-        int BottomPosition;
-
-        double armPosition = 0.2;
-        double armDownPosition = 0.7;
-        double armUpPosition = 0.8;
-        leftArm.setPosition(armUpPosition);
-
-//        double clawPosition = 0.0;
-        double clawOpenPosition = 0.5;
-        double clawClosedPosition = 1.0;
-        Claw.setPosition(clawClosedPosition);
-
-        double elbowUpPosition = 0.55;
-        double elbowDownPosition = 0.25;
-        Elbow.setPosition(elbowUpPosition);
-
-        double elbowPosition = 0.0;
-//        Elbow.setPosition(elbowPosition);
-//
-        double wristPosition = 0.0;
-//        Wrist.setPosition(wristPosition);
-
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        TiltUpPosition = 5;
-        TiltDownPosition = -665;
-        TopPosition = -1600;
-        BottomPosition = 10;
+        robot.init(hardwareMap);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            //Linkage Programming
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
 
-            if (gamepad1.x) {
-                tiltMotor.setPower(0.5);
-                tiltMotor.setTargetPosition(TiltUpPosition);
-                tiltMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            moveRobot();
+
+            moveSliders();
+
+            if (gamepad1.dpad_right) {
+                robot.claw.setPosition(robot.clawOpenPosition);
             }
-
-            if (gamepad1.b) {
-                tiltMotor.setPower(0.3);
-                tiltMotor.setTargetPosition(TiltDownPosition);
-                tiltMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            }
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-
-            // Get the current position of the armMotor
-            double position = tiltMotor.getCurrentPosition();
-
-            // Get the target position of the armMotor
-            double desiredPosition = tiltMotor.getTargetPosition();
-
-            // Show the position of the tiltMotor on telemetry
-            telemetry.addData("Encoder Position", position);
-
-            // Show the target position of the tiltMotor on telemetry
-            telemetry.addData("Desired Position", desiredPosition);
-
-            //Sliders Programming
-            if (gamepad1.y) {
-                sliderMotor.setTargetPosition(TopPosition);
-                sliderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                sliderMotor.setPower(0.5);
-
-            }
-
-            if (gamepad1.a) {
-                sliderMotor.setTargetPosition(BottomPosition);
-                sliderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                sliderMotor.setPower(0.3);
-            }
-//
-
-            // GOOD
-            if (gamepad1.dpad_up) {
-                leftArm.setPosition(armUpPosition);
-            } else if (gamepad1.dpad_down) {
-                leftArm.setPosition(armDownPosition);
-            }
-
-            // GOOD
-            if (gamepad1.dpad_left) {
-                Claw.setPosition(clawClosedPosition);
-            } else if (gamepad1.dpad_right) {
-                Claw.setPosition(clawOpenPosition);
-            }
-
-//            telemetry.addData("claw position", clawPosition);
 
             if (gamepad1.left_bumper) {
-                Elbow.setPosition(elbowUpPosition);
+                grabAndTiltUpCombo();
             } else if (gamepad1.right_bumper) {
-                Elbow.setPosition(elbowDownPosition);
+                tiltDownCombo();
             }
+//            if (gamepad1.x) {
+//                tiltMotor.setPower(0.5);
+//                tiltMotor.setTargetPosition(TiltUpPosition);
+//                tiltMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            }
+//
+//            if (gamepad1.b) {
+//                tiltMotor.setPower(0.3);
+//                tiltMotor.setTargetPosition(TiltDownPosition);
+//                tiltMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            }
 
-            telemetry.addData("elbow position: ", elbowPosition);
 //
 //            if (gamepad1.dpad_left) {
 //                wristPosition += 0.05;
@@ -164,23 +63,70 @@ public class OutreachTeleop extends LinearOpMode {
 //                }
 //            }
 
-            telemetry.addData("wrist position: ", wristPosition);
-
-            // Get the current position of the armMotor
-            double position2 = sliderMotor.getCurrentPosition();
-
-            // Get the target position of the armMotor
-            double desiredPosition2 = sliderMotor.getTargetPosition();
-
-            // Show the position of the tiltMotor on telemetry
-            telemetry.addData("Slider Encoder Position", position2);
-
-            // Show the target position of the tiltMotor on telemetry
-            telemetry.addData("Slider Desired Position", desiredPosition2);
-
             telemetry.update();
-
-
         }
+    }
+
+    private void moveRobot() {
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        robot.frontLeftMotor.setPower(frontLeftPower * speedLimit);
+        robot.backLeftMotor.setPower(backLeftPower * speedLimit);
+        robot.frontRightMotor.setPower(frontRightPower* speedLimit);
+        robot.backRightMotor.setPower(backRightPower * speedLimit);
+    }
+
+    private void moveSliders() {
+
+        // Check to see if we are close enough to the current target
+        int currentPosition = robot.sliderMotor.getCurrentPosition();
+        if (Math.abs(currentPosition - sliderCurrentPosition) <= 10) {
+            robot.sliderMotor.setPower(0.0);
+        }
+        telemetry.addData("debug currentPosition", currentPosition);
+        telemetry.addData("debug sliderCurrentPosition", sliderCurrentPosition);
+
+        if (gamepad1.y) {
+            sliderCurrentPosition = robot.sliderTopPosition;
+            robot.sliderMotor.setTargetPosition(sliderCurrentPosition);
+            robot.sliderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.sliderMotor.setPower(0.5);
+        }
+
+        if (gamepad1.a) {
+            sliderCurrentPosition = robot.sliderBottomPosition;
+            robot.sliderMotor.setTargetPosition(sliderCurrentPosition);
+            robot.sliderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.sliderMotor.setPower(0.3);
+        }
+
+        telemetry.addData("Slider Encoder Position", robot.sliderMotor.getCurrentPosition());
+        telemetry.addData("Slider Desired Position", robot.sliderMotor.getTargetPosition());
+    }
+
+    private void tiltDownCombo() {
+        robot.elbow.setPosition(robot.elbowDownPosition);
+        robot.claw.setPosition(robot.clawOpenPosition);
+        robot.rightArm.setPosition(robot.armDownPosition);
+    }
+
+    private void grabAndTiltUpCombo() throws InterruptedException {
+        robot.rightArm.setPosition(robot.armDownPosition + .05);
+        Thread.sleep(500);
+        robot.claw.setPosition(robot.clawClosedPosition);
+        Thread.sleep(500);
+        robot.elbow.setPosition(robot.elbowUpPosition);
+        robot.rightArm.setPosition(robot.armUpPosition);
     }
 }
