0bae3f98b8adc39a2c7d3475824f501e3ed5b414
Comiccheese <147282330+Comiccheese@users.noreply.github.com>
2025-05-24 16:04:32 -0700

    add slider motor pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/Config.java
@@ -14,26 +14,26 @@ public final class Config {
     public static final String SLIDER_MOTOR_1 = "slidesMotor1";
     public static final String SLIDER_MOTOR_2 = "slidesMotor2";
     public static final String TILT_MOTOR = "TiltMotor";
-    public static final double WRIST_INIT_POS = 0.5;
-    public static final double ARM_UP_POSITION = 0.42;
-    public static final double ARM_DOWN_POSITION = 0.50;
-    public static final double ARM_DOWN_GRAB_POSITION = 0.62;
-    public static final double ARM_WALL_INTAKE_POSITION = 0.20;
-
-    public static final double CLAW_OPEN_POSITION = 0.35;
-    public static final double CLAW_CLOSED_POSITION = 0.62;
-
-    public static final double ELBOW_UP_POSITION = 0.65;
-    public static final double ELBOW_MID_POSITION = 0.35;
-    public static final double ELBOW_DOWN_POSITION = 0.95;
-    public static final double ELBOW_CLIP_INTAKE_POSITION = 0.75;
-
-    public static final int TILT_UP_POSITION = 970;
-    public static final int TILT_DOWN_POSITION = 0;
+//    public static final double WRIST_INIT_POS = 0.5;
+//    public static final double ARM_UP_POSITION = 0.42;
+//    public static final double ARM_DOWN_POSITION = 0.50;
+//    public static final double ARM_DOWN_GRAB_POSITION = 0.62;
+//    public static final double ARM_WALL_INTAKE_POSITION = 0.20;
+//
+//    public static final double CLAW_OPEN_POSITION = 0.35;
+//    public static final double CLAW_CLOSED_POSITION = 0.62;
+//
+//    public static final double ELBOW_UP_POSITION = 0.65;
+//    public static final double ELBOW_MID_POSITION = 0.35;
+//    public static final double ELBOW_DOWN_POSITION = 0.95;
+//    public static final double ELBOW_CLIP_INTAKE_POSITION = 0.75;
+//
+//    public static final int TILT_UP_POSITION = 970;
+//    public static final int TILT_DOWN_POSITION = 0;
 
     public static int SLIDER_TOP_POSITION = -550;
     public static int SLIDER_BOTTOM_POSITION = 0;
-    public static int SLIDER_CLOSE_INTAKE_POSITION = -30;
-    public static int SLIDER_CLIP_DELIVERY_PREP_POSITION = -515;
-    public static int SLIDER_CLIP_DELIVERY_POSITION = -250;
+//    public static int SLIDER_CLOSE_INTAKE_POSITION = -30;
+//    public static int SLIDER_CLIP_DELIVERY_PREP_POSITION = -515;
+//    public static int SLIDER_CLIP_DELIVERY_POSITION = -250;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/RobotHardware.java
@@ -5,6 +5,10 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.common.Odometry.SparkFunOdometry;
 import org.firstinspires.ftc.teamcode.common.subsystem.IntakeDeliverySubsystem;
 import org.firstinspires.ftc.teamcode.common.util.wrappers.ServoActuator;
